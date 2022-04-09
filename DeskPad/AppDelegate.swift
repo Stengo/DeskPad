c0bba5146f9cab710a191fc2987ec27253e929b1
Stengo <bastian.andelefski@googlemail.com>
2022-04-09 11:24:38 +0200

    Remove unnecessary stuff

--- DeskPad/AppDelegate.swift
+++ DeskPad/AppDelegate.swift
@@ -1,30 +1,7 @@
-//
-//  AppDelegate.swift
-//  DeskPad
-//
-//  Created by Bastian Andelefski on 09.04.22.
-//
-
 import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
-
-
 }
-
