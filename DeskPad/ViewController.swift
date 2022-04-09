592349336b5fcf80afabb4f63d5d6195bd007105
Stengo <bastian.andelefski@googlemail.com>
2022-04-09 11:21:09 +0200

    Initial Commit

---
+++ DeskPad/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DeskPad
+//
+//  Created by Bastian Andelefski on 09.04.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
