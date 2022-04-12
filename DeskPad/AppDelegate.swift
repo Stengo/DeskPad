14cb6738673af77cbac4cafa748c611d42c7094e
Stengo <bastian.andelefski@googlemail.com>
2022-04-12 19:27:12 +0200

    Enable quitting via cmd+q

--- DeskPad/AppDelegate.swift
+++ DeskPad/AppDelegate.swift
@@ -13,6 +13,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.contentMaxSize = contentSize
         window.setContentSize(contentSize)
         window.center()
+
+        let mainMenu = NSMenu()
+        let mainMenuItem = NSMenuItem()
+        let subMenu = NSMenu(title: "MainMenu")
+        let quitMenuItem = NSMenuItem(
+            title: "Quit",
+            action: #selector(NSApp.terminate),
+            keyEquivalent: "q"
+        )
+        subMenu.addItem(quitMenuItem)
+        mainMenuItem.submenu = subMenu
+        mainMenu.items = [mainMenuItem]
+        NSApplication.shared.mainMenu = mainMenu
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
