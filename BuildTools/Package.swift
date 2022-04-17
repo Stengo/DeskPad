274df76a47dfb0ebbdf07c602ffa6261dbf6b855
Stengo <bastian.andelefski@googlemail.com>
2022-04-17 12:04:25 +0200

    Add SwiftFormat

---
+++ BuildTools/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "BuildTools",
+    platforms: [.macOS(.v10_11)],
+    dependencies: [
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.12"),
+    ],
+    targets: [.target(name: "BuildTools", path: "")]
+)
